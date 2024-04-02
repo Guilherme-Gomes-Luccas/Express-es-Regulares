8737ad4e708d07d6f122d8402ce7c41c2c46fc2e
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-04-02 13:14:59 -0300

    Trabalhando com quantificadores

--- busca_telefone.js
+++ busca_telefone.js
@@ -3,9 +3,16 @@ const fs = require('fs')
 const bancoCsv = 'database.csv'
 const banco = fs.readFileSync(bancoCsv, "utf-8")
 
-const regexTelefone = /\(\d+\)\s\d+-\d+/g 
+const regexTelefone = /\(\d{2}\)\s\d{4,5}-\d{4}/g
+/*const regexTelefone = /\(\d+\)\s\d+-\d+/g*/
 /*\d+ -> Vai receber uma sequencia númerica */
 /*g --> faz com que a regex pegue toda a sequencia e a guarde dentro da variavel*/
 
 const matchTelefone = banco.match(regexTelefone)
 console.log(matchTelefone)
+
+const patternCel = /\(\d{2}\)\s\d{5}-\d{4}/g
+/*Usamos um quantificador para definir exatamente o número de digitos*/
+
+const matchCeluar = banco.match(patternCel)
+console.log(matchCeluar);
